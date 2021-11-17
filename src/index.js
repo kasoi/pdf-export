0de3c798dd70c9970b4030b98591cdfbaf7cbc83
iamblackornot <nkzshinnnik@gmail.com>
2021-11-17 18:14:57 +0300

    -

--- src/index.js
+++ src/index.js
@@ -67,7 +67,8 @@ const getImageOptions = (width, ratio, dpi) => {
     width : Math.round(width),
     height : Math.round(width / ratio),
     density : dpi,
-    format : 'jpg'
+    format : 'jpg',
+    quality : 100
   };
 
   return options;
