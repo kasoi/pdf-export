9ded9f4362e2a7d17b5fb22c8bb34028dbc5d716
iamblackornot <nkzshinnnik@gmail.com>
2021-11-28 17:50:18 +0300

    -

--- src/index.js
+++ src/index.js
@@ -319,7 +319,7 @@ app.get('/size', async (req, res) => {
 app.get('/convert', async (req, res) => {
 
   //const path = `./assets/STPE20.pdf`;
-  const path = `./assets/cert.pdf`;
+  const path = `./assets/work.pdf`;
   console.log(path);
 
   let pdfParser = new PDFParser();
