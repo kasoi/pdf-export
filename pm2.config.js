24ad61712ab4478c8ee3cdeb8b2b47ff94f1b005
iamblackornot <nkzshinnnik@gmail.com>
2021-12-04 20:53:31 +0300

    -

--- pm2.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps : [{
-      name: "pdf-export",
-      script: "npm start",
-      instances: 1,
-      cron_restart: "0 0 * * *",
-      max_memory_restart: "300M"
-    }]
-  }
