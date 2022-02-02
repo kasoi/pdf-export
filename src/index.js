6cc2f5bae1fc7c2b2a627ca721f90bf9ffe97083
iamblackornot <nkzshinnnik@gmail.com>
2022-02-02 13:15:10 +0300

    -

--- src/index.js
+++ src/index.js
@@ -434,7 +434,8 @@ app.get('/', (req, res) => {
 app.get('/convert', async (req, res) => {
 
   //const path = `./assets/STPE20.pdf`;
-  const path = `./assets/Kaiser-Permanente-Advance-Health-Care-Directive.pdf`;
+  //const path = `./assets/Kaiser-Permanente-Advance-Health-Care-Directive.pdf`;
+  const path = `./assets/small-image-test.pdf`;
   console.log(path);
 
   const existingPdfBytes = fs.readFileSync(path);
