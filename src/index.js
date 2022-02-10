ba94335e452684a172ee5f873fc509213b6c5c5a
iamblackornot <nkzshinnnik@gmail.com>
2022-02-10 20:48:40 +0300

    -

--- src/index.js
+++ src/index.js
@@ -491,6 +491,7 @@ app.post('/size', async (req, res) => {
     }
     catch (err) {
 
+      console.log(err);
       throw new GenerateThumbnailError('failed to generate thumbnail', err.code === 'ENOMEM');
     }
 
